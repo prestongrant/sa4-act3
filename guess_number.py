1d3eeea159832be112e16552f0f7f7d73cb4fa71
Preston Armstrong <pgaxbox@gmail.com>
2024-02-06 22:01:04 -0600

    Modified guess number check to include continuation of guesses and a quit option

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,21 @@
 number = 10
-
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
-    print("Congratulations! You guessed the right number.")
-else:
-    print(f"Sorry! The number was {number}.")
+while True:
+    guess_input = input("What number is correct? (Enter 'q' to quit) ")
+
+    if guess_input.lower() == 'q':
+        print(f"The number is {number}.")
+        break
+
+    try:
+        guess = int(guess_input)
+    except ValueError:
+        print("Wrong guess. Please try again or 'q' to quit.")
+        continue
+
+    if guess == number:
+        print("Congratulations! You guessed the right number.")
+        break
+    else:
+        print(f"Sorry! The number was {number}.")
