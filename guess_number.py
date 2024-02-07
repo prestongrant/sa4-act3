54633d55d557a6045c05335191a91f91b5ef4c59
Preston Armstrong <pgaxbox@gmail.com>
2024-02-06 22:17:21 -0600

    Modified file where user had a limited amount of guesses until the correct one

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,10 @@
 number = 10
+max_guesses = 3
 print("I'm thinking of a number...")
 
-while True:
+guess_count = 0
+
+while guess_count < max_guesses:
     guess_input = input("What number is correct? (Enter 'q' to quit) ")
 
     if guess_input.lower() == 'q':
@@ -14,8 +17,15 @@ while True:
         print("Wrong guess. Please try again or 'q' to quit.")
         continue
 
+    guess_count += 1
+
     if guess == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif guess < number:
+        print("Sorry, your guess was too low>")
     else:
-        print(f"Sorry! The number was {number}.")
+        print("Sorry, your guess was too high.")
+
+if guess_count == max_guesses:
+    print("Sorry, you ran out of guesses. The correct number is {number}.")
